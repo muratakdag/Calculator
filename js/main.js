38e699b238355886eb83cc5d7b39ff056f7e926e
muratakdag <bafilonbambam@gmail.com>
2021-04-05 14:54:37 +0300

    Update js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Start writing JavaScript
+// Start writing JavaScript here!
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
 const display = calculator.querySelector(".calculator__display");
@@ -9,18 +9,45 @@ keys.addEventListener("click", (event) => {
   const key = event.target;
   const keyValue = key.textContent;
   const displayValue = display.textContent;
+  const { type } = key.dataset;
+  const { previousKeyType } = calculator.dataset;
 
-  //  Is this a number key?
-  if (key.classList.contains("number")) {
-    if (displayValue === "0") {
+  if (type === "number") {
+    if (displayValue === "0" || previousKeyType === "operator") {
       display.textContent = keyValue;
     } else {
       display.textContent = displayValue + keyValue;
     }
   }
 
-  //  Is this an operator key?
-  if (key.classList.contains("operator")) {
-    calculator.dataset.previousKeyType = "operator";
+  if (type === "operator") {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach((el) => {
+      el.dataset.state = "";
+    });
+    key.dataset.state = "selected";
+
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
   }
+
+  if (type === "equal") {
+    // Perform a calculation
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    const secondNumber = displayValue;
+    display.textContent = calculate(firstNumber, operator, secondNumber);
+  }
+
+  calculator.dataset.previousKeyType = type;
 });
+
+function calculate(firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+
+  if (operator === "plus") return firstNumber + secondNumber;
+  if (operator === "minus") return firstNumber - secondNumber;
+  if (operator === "times") return firstNumber * secondNumber;
+  if (operator === "divide") return firstNumber / secondNumber;
+}
